f0321ad09f7e001ad91925a53577cb4aedec2690
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-31 01:40:08 +0530

    Added: Insert in binary tree Program

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+public class BinarySearchTree {
+	MyBinaryNode root;
+	public void insertData(int data) {
+		root = insert(root,data);
+		}
+	public MyBinaryNode insert(MyBinaryNode root, int data) {
+		MyBinaryNode newNode = new MyBinaryNode();
+		newNode.data = data;
+		newNode.left = null;
+		newNode.right = null;
+		if (root == null) {
+			root =newNode;
+			return root;
+		}
+		if (root.data < newNode.data) {
+			root.right = insert(root.right, data);
+		}
+		else if (root.data > newNode.data) {
+			root.left = insert(root.left, data);
+		}
+		return root;
+	}
+}
---
+++ src/com/bridgelabz/MyBinaryNode.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class MyBinaryNode {
+	int data;
+	public MyBinaryNode left;
+	public MyBinaryNode right;
+	
+	
+	public MyBinaryNode() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public MyBinaryNode(int data, MyBinaryNode left, MyBinaryNode right) {
+		super();
+		this.data = data;
+		this.left = left;
+		this.right = right;
+	}
+}
